c021cee5d4b332d09917702f3a76e18d4a6d0163
younesfakallah <senyoupl@icloud.com>
2022-07-30 13:49:24 +0200

    Avancement -> Reception + intégration de la température en celcius, intégration de la card météo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 // Changement global de la langue pour l'utilisation francaise de momentjs
 moment.locale('fr');
 
+// Animation sans timeline avec la librairie GSAP
+gsap.fromTo(".cloud", {x: 500}, {x: 100, duration: 2});
+gsap.fromTo(".cloud", {scaleX:0.5, scaleY:0.5}, {scaleX:1, scaleY:1, duration:2});
+gsap.fromTo(".explanation", {y: 100, autoAlpha: 0}, {y: 10, autoAlpha: 1, duration: 2});
+gsap.fromTo(".fa-share", {y: 10, autoAlpha: 0}, {y: 0, autoAlpha: 1, duration: 2});
+
 // Récupération des éléments du DOM
 const input = document.querySelector('.input-text');
 const inputSend = document.querySelector('.btn-absolute');
 const errorMsg = document.querySelector('.error-message');
+const celcius = document.querySelector('.celcius');
 
 // Récupération de la date du jour + ajout dans le DOM
 const date = moment().format('dddd DD MMMM');
@@ -22,15 +29,15 @@ inputSend.addEventListener('click', (e) => {
     weatherBalloon(input.value);
 });
 
-// Cette fonction contient une animation.
+// Cette fonction créer une animation
 // Elle est appellée dans le cas ou un message d'erreur est nécessaire
 const errFunc = () => gsap.fromTo(".error-message", {y: 10, display: 'block', autoAlpha: 0}, {y: 0, autoAlpha: 1, duration: 2});
 
 
 
 const weatherBalloon = ( cityName ) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
-    .then(function(resp) { return resp.json() }) // convertion en donnée
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`)
+    .then(function(resp) { return resp.json() }) // convertion en donnée json
     .then(function(data) {
       if(data.message == 'Nothing to geocode') { 
             errorMsg.innerHTML = '<p>Merci de saisir le nom d\'une ville avant de soumettre</p>';
@@ -38,19 +45,18 @@ const weatherBalloon = ( cityName ) => {
       } else if(data.message == 'city not found') {
             errorMsg.innerHTML = '<p>Cette ville est introuvable</p>';
             errFunc();
+      } else {
+        gsap.to(".cloud", {y: -200, autoAlpha: 0, duration: 3});
+        gsap.to(".explanation", {autoAlpha: 0});
+        gsap.to(".form-horizontal", {y: -280});
+        gsap.fromTo(".card", {autoAlpha: 0, y: 100}, {y: 60, autoAlpha: 1});
+        celcius.textContent = Math.round(data.main.temp) + '°';
       }
     })
     .catch(function(error) {
-      errorMsg.innerHTML = error;
+      errorMsg.innerHTML = `<p>${error}</p>`;
       errFunc();
     });
   }
-
-
-// Animation avec la librairie GSAP
-gsap.fromTo(".cloud", {x: 500}, {x: 100, duration: 5});
-gsap.fromTo(".cloud", {scaleX:0.5, scaleY:0.5}, {scaleX:1, scaleY:1, duration:2});
-gsap.fromTo(".explanation", {y: 100, autoAlpha: 0}, {y: 10, autoAlpha: 1, duration: 2});
-gsap.fromTo(".fa-share", {y: 10, autoAlpha: 0}, {y: 0, autoAlpha: 1, duration: 2});
 
 
