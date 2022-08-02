e958cf1885265c71a94e2e1921a869965e991a18
younesfakallah <senyoupl@icloud.com>
2022-08-02 14:48:45 +0200

    Amélioration -> ajout d'un système de géolocalisation grâce à l'api geocode de google

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ let tl = gsap.timeline({duration:1});
 tl.to(".cloud", {x: 100});
 tl.fromTo(".cloud", {scaleX:0.5, scaleY:0.5}, {scaleX:1, scaleY:1});
 tl.fromTo(".explanation", {y: 100, autoAlpha: 0}, {y: 10, autoAlpha: 1});
+tl.fromTo(".fa-street-view", {y: 10, autoAlpha: 0}, {y: 0, autoAlpha: 1});
 tl.fromTo(".fa-share", {y: 10, autoAlpha: 0}, {y: 0, autoAlpha: 1});
 
 // Récupération des éléments du DOM
@@ -14,14 +15,16 @@ const inputSend = document.querySelector('.btn-absolute');
 const errorMsg = document.querySelector('.error-message');
 const celcius = document.querySelector('.celcius');
 const description = document.querySelector('.description');
+const getuserLocation = document.querySelector('.fa-street-view');
 
 // Récupération de la date du jour + intégration dans le DOM
 const date = moment().format('dddd DD MMMM');
 const dateField = document.getElementById('date');
 dateField.textContent = date;
 
-//Stockage de la clée API
-let apiKey = "15cdb3b6d112afb5d24ccedff1409ddb";
+//Stockage des clées API
+let owmapiKey = "15cdb3b6d112afb5d24ccedff1409ddb";
+let geoapiKey = "AIzaSyAJu_s1CyPYI990lEIHpP1T5C3X64TEjjY";
 
 
 
@@ -38,7 +41,7 @@ const errFunc = () => gsap.fromTo(".error-message", {y: 10, display: 'block', au
 
 // Cette fonction sert à appeler l'api, en fonction du paramétre donnée lors de l'appel (la ville) elle renverra les donnée météo de cette dernière
 const weatherBalloon = ( cityName ) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=fr&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=fr&appid=${owmapiKey}`)
     .then(function(resp) { return resp.json() }) // convertion en donnée json
     .then(function(data) {
       if(data.message == 'Nothing to geocode') { 
@@ -66,4 +69,19 @@ const weatherBalloon = ( cityName ) => {
     });
   }
 
+  // Cette fonction retourne le nom de la ville de l'utilisateur automatiquement grâce à l'ape geocode de google
+  // ce nom sera donné en paramétre à la fonction weatherballoon
+  getuserLocation.addEventListener('click', () => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        fetch( "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +`&sensor=false&key=${geoapiKey}`)
+        .then(function(resp) { return resp.json() })
+        .then(function(data) { weatherBalloon(data.results[0].address_components[2].long_name) }) 
+
+      });
+    } else {
+      errorMsg.innerHTML = `<p>La géolocalisation est soit désactiver soit non supporter par votre navigateur</p>`;
+      errFunc();
+    }
+  })
 
